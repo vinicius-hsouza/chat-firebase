d8d58764a9ace12579daef271346f86814cc4685
Vinicius Henrique de Souza <vini.henrique2501@gmail.com>
2024-09-15 23:28:28 -0300

    novo estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import io from 'socket.io-client'
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 
-import Input from './components/Input';
-import { IoIosLogOut, IoMdSend } from 'react-icons/io';
+// import Input from './components/Input';
+import { IoIosLogOut, IoMdSend, IoMdHappy, IoLogoGoogle } from 'react-icons/io';
 
 import { initializeApp } from 'firebase/app';
 import { collection, getFirestore, query, orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
@@ -12,6 +12,16 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import './styles/global.css'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGRBJ_lBuNjbhfJsB4WaPLdnK0hv43UZo",
@@ -30,10 +40,23 @@ const auth = getAuth(appFirebase);
 import GlobalStyles from './styles/global';
 import { Container, Content, Header, EditorContent, MessagesContent, MessageItem } from './styles/chat';
 import { format, parseISO } from 'date-fns';
+import { Button } from './components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader } from './components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from './components/ui/avatar';
+import { Separator } from './components/ui/separator';
+import { Input } from './components/ui/input';
+import EmojiPicker from 'emoji-picker-react';
+import { PiPProvider, usePiPWindow } from './contextPIP';
+import { LogOut, PictureInPicture } from 'lucide-react';
+import PiPWindow from './components/PIPWindow';
+
 
 function App() {
   const formRef = useRef<FormHandles>(null);
   const [user] = useAuthState(auth);
+  const [messageText, setMessageText] = useState('')
+  const { isSupported, requestPipWindow, pipWindow, closePipWindow } =
+    usePiPWindow();
 
 
   const messagesCollection = collection(getFirestore(appFirebase), 'messages');
@@ -53,13 +76,12 @@ function App() {
       const response = await addDoc(messagesCollection, {
         uid: user?.uid,
         username: user?.displayName,
-        text: data.text,
+        text: messageText,
         photoURL: user?.photoURL,
         created_at: serverTimestamp(),
       })
 
-      console.log(response);
-      formRef.current?.reset()
+      setMessageText('')
     } catch (err) {
       console.log(err);
     }
@@ -76,69 +98,171 @@ function App() {
 
     return (
       <>
-        <GlobalStyles />
-        <Container>
-          <button type='button' onClick={singInGoogle}>logar</button>
-        </Container>
+        <div className="flex flex-1 items-center justify-center h-screen">
+          {/* <button type='button' onClick={singInGoogle}>logar</button> */}
+          <Button onClick={singInGoogle} className="gap-2"><IoLogoGoogle />Acessar com o google</Button>
+        </div>
       </>
     )
   }
 
+  const addEmoji = (e: any) => {
+    let sym = e.unified.split("-");
+    let codesArray: any = [];
+    sym.forEach((el: any) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setMessageText(emoji);
+  };
+
+
+
   return (
     <>
-      <GlobalStyles />
-      <Container>
-        <Content>
-          <Header>
-            <img src={user?.photoURL || undefined} alt="userAvatar" />
-            <div>
-              <p>{user?.displayName}</p>
-              <p>{user?.email}</p>
+      {/* <GlobalStyles /> */}
+      <div className="flex flex-1 items-center justify-center h-screen">
+        <Card className='h-[600px] w-[400px] flex flex-col'>
+          <CardHeader>
+            <div className="flex flex-row w-full gap-2">
+              <Avatar>
+                <AvatarImage src={user?.photoURL as string} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className='flex flex-1 flex-col'>
+                <p className='text-sm font-bold'>{user?.displayName}</p>
+                <p className='text-sm font-medium text-gray-600'>{user?.email}</p>
+              </div>
+              <span className="gap-2 flex">
+
+                <Button onClick={() => requestPipWindow(400, 600)} size="icon" variant="outline">
+                  <PictureInPicture size="14px" />
+                </Button>
+                <Button onClick={() => signOut(auth)} size="icon" variant="outline">
+                  <LogOut size="14px" />
+                </Button>
+              </span>
             </div>
-            <span>
-              <div onClick={() => signOut(auth)}>
-                <IoIosLogOut />
-              </div>
-            </span>
-          </Header>
-          <MessagesContent id="message_content">
+          </CardHeader>
+          <Separator />
+          <CardContent id='message_content' className={`flex flex-1 flex-col overflow-auto py-4 gap-2`}>
             {messages?.map(message => (
-              <MessageItem isUserLogged={message.uid === user?.uid}>
-                {message.uid !== user?.uid && <img src={message.photoURL} alt="userAvatar" />}
-                {<div >
-                  {message.uid !== user?.uid ? <p>{message.username}</p> : <p></p>}
-                  <p>{message.text}</p>
-                  {message.created_at?.seconds && <span>
-                    <p>{format(new Date(message.created_at.seconds * 1000), 'HH:mm')}</p>
-                  </span>}
-                </div>}
+              <>
+                <div className={`flex gap-1 max-w-[60%] ${message.uid === user?.uid && 'self-end'}`}>
+                  {message.uid !== user?.uid && (
+                    <Avatar>
+                      <AvatarImage src={message.photoURL as string} />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                  )}
+                  <div>
 
-              </MessageItem>
+                    <div className={`flex flex-col flex-1 p-2 rounded-lg ${message.uid === user?.uid ? 'bg-primary text-primary-foreground' : 'bg-muted'}`}>
+                      <p className="text-xs font-semibold ">{message.uid !== user?.uid ? message.username : 'Você'}</p>
+                      <div className="flex justify-between items-baseline gap-2">
+                        <p className="text-sm font-normal">{message.text}</p>
+                      </div>
+                    </div>
+                    {message.created_at?.seconds && <span className="self-end w-full justify-end flex">
+                      <p className='text-[8px] font-medium text-gray-600'>{format(new Date(message.created_at.seconds * 1000), 'dd/MM HH:mm')}</p>
+                    </span>}
+                  </div>
+                </div>
+
+              </>
             ))}
-          </MessagesContent>
-          <EditorContent>
-            <Form ref={formRef} onSubmit={handleSubmit}>
-              <Input name="text" placeholder="digite aqui" />
-              <div onClick={() => formRef.current?.submitForm()}>
-                <IoMdSend />
-              </div>
-            </Form>
-          </EditorContent>
-          {/* 
-          {!user ? (
-            <button type='button' onClick={singInGoogle}>logar</button>
-          ) : (
-            <>
+          </CardContent>
+          <Separator />
+          <CardFooter className='flex w-full items-center gap-2 p-2'>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button size="icon" variant="outline">
+                  <IoMdHappy />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <EmojiPicker onEmojiClick={e => addEmoji(e)} />
+              </DropdownMenuContent>
+            </DropdownMenu>
 
-              <Form ref={formRef} onSubmit={handleSubmit}>
-                <Input name="text" />
-                <button type="submit">enviar</button>
-              </Form>
-              <button type='button' onClick={() => signOut(auth)}>deslogar</button>
-            </>
-          )} */}
-        </Content>
-      </Container>
+            <Input className="flex-1" placeholder='Digite sua menssagem aqui' onChange={e => setMessageText(e.target.value)} />
+            <Button size="icon" disabled={!messageText} onClick={handleSubmit}>
+              <IoMdSend />
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      {pipWindow && (<PiPWindow pipWindow={pipWindow}>
+        <div className="flex flex-1">
+          <Card className='h-[600px] w-[400px] flex flex-col'>
+            <CardHeader>
+              <div className="flex flex-row w-full gap-2">
+                <Avatar>
+                  <AvatarImage src={user?.photoURL as string} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div className='flex flex-1 flex-col'>
+                  <p className='text-sm font-bold'>{user?.displayName}</p>
+                  <p className='text-sm font-medium text-gray-600'>{user?.email}</p>
+                </div>
+                <span className="gap-2 flex">
+
+                  <Button onClick={() => closePipWindow()} size="icon" variant="outline">
+                    <PictureInPicture size="14px" style={{ 'transform': 'rotate(180deg)' }} />
+                  </Button>
+                  {/* <Button onClick={() => signOut(auth)} size="icon" variant="outline">
+                    <LogOut size="14px" />
+                  </Button> */}
+                </span>
+              </div>
+            </CardHeader>
+            <Separator />
+            <CardContent id='message_content' className={`flex flex-1 flex-col overflow-auto py-4 gap-2`}>
+              {messages?.map(message => (
+                <>
+                  <div className={`flex gap-1 max-w-[60%] ${message.uid === user?.uid && 'self-end'}`}>
+                    {message.uid !== user?.uid && (
+                      <Avatar>
+                        <AvatarImage src={message.photoURL as string} />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    )}
+                    <div>
+
+                      <div className={`flex flex-col flex-1 p-2 rounded-lg ${message.uid === user?.uid ? 'bg-primary text-primary-foreground' : 'bg-muted'}`}>
+                        <p className="text-xs font-semibold ">{message.uid !== user?.uid ? message.username : 'Você'}</p>
+                        <div className="flex justify-between items-baseline gap-2">
+                          <p className="text-sm font-normal">{message.text}</p>
+                        </div>
+                      </div>
+                      {message.created_at?.seconds && <span className="self-end w-full justify-end flex">
+                        <p className='text-[8px] font-medium text-gray-600'>{format(new Date(message.created_at.seconds * 1000), 'dd/MM HH:mm')}</p>
+                      </span>}
+                    </div>
+                  </div>
+
+                </>
+              ))}
+            </CardContent>
+            <Separator />
+            <CardFooter className='flex w-full items-center gap-2 p-2'>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button size="icon" variant="outline">
+                    <IoMdHappy />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <EmojiPicker onEmojiClick={e => addEmoji(e)} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <Input className="flex-1" placeholder='Digite sua menssagem aqui' onChange={e => setMessageText(e.target.value)} value={messageText} />
+              <Button size="icon" disabled={!messageText} onClick={handleSubmit}>
+                <IoMdSend />
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </PiPWindow>)}
     </>
 
   )
